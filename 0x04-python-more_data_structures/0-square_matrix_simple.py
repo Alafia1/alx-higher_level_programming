ab6fd6fb3818c7e720039f10616735d35d7c4437
Alafia1 <101114171+Alafia1@users.noreply.github.com>
2023-01-11 12:09:43 +0100

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,11 @@ def square_matrix_simple(matrix=[]):
     '''
         A function that returns the square value of all elements in a
         multi dimensional list been passed as arguments.
+
+
         Returns a new matrix that is of same size as the size of the
         matrix passed to it as arguments.
+
         You are allowed to use regular loops, map, etc
     '''
     new_lst = []
@@ -12,4 +15,4 @@ def square_matrix_simple(matrix=[]):
         return new_lst
 
     new_lst = [[i*i for i in j] for j in matrix]
-    return 
+    return new_lst
